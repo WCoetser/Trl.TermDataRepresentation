49864c0a649bb0dd08cf5442bd2cda7c8f5cc443
Wikus Coetser <spring.haas.meester@gmail.com>
2020-09-05 12:19:17 +1000

    Publish, update version

--- publish.ps1
+++ publish.ps1
@@ -1,3 +1,2 @@
 
-dotnet pack ./Trl.Serialization/Trl.Serialization.csproj --output .\nupkg --configuration Release
 dotnet pack ./Trl.TermDataRepresentation/Trl.TermDataRepresentation.csproj --output .\nupkg --configuration Release
